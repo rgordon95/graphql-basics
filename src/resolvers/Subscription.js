b017f8151cfd3046e569d342ed359c3ec1bdfcdd
Richard Gordon <richgordon95@gmail.com>
2020-09-27 14:16:15 -0400

    cleanup count sub

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,20 +1,6 @@
 import { locales } from '../locales';
 
 const Subscription = {
-  count: {
-      subscribe(parent, args, { pubsub }, info) {
-        let count = 0;
-
-        setInterval(() => {
-            count++;
-            pubsub.publish('count', {
-                count,
-            })
-        }, 1000)
-
-        return pubsub.asyncIterator('count')
-      }
-  },
   comment: {
     subscribe(parent, { postId }, { db, pubsub }, info ) {
         const post = db.posts.find((post) => post.id === postId && post.published)
@@ -27,12 +13,12 @@ const Subscription = {
 
   }
 },
-post: {
+  post: {
     subscribe(parent, args, { pubsub }, info ) {
 
         return pubsub.asyncIterator(`post`)
     }
-}
+  }
 }
 
 export { Subscription as default }
